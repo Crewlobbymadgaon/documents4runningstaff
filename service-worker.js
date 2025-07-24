c2a2dfb87d12e87aafc5a59ad18d73f20261b264
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2025-07-24 08:41:12 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,38 +11,40 @@ const urlsToCache = [
   '/documents4runningstaff/styles.css',
   '/documents4runningstaff/img/shedorder.png',
   '/documents4runningstaff/img/safety.png',
-  // add more assets here as needed
+  // Add all HTML, images, and PDF files you expect offline
 ];
 
-// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-// Activate event
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, { ignoreSearch: true }).then(response => {
       return response || fetch(event.request).catch(() => {
         if (event.request.destination === 'document') {
           return caches.match('/documents4runningstaff/offline.html');
+        } else if (event.request.destination === 'image') {
+          return caches.match('/documents4runningstaff/img/placeholder.png');
+        } else if (event.request.destination === 'style') {
+          return new Response('', { headers: { 'Content-Type': 'text/css' } });
+        } else if (event.request.destination === 'script') {
+          return new Response('', { headers: { 'Content-Type': 'application/javascript' } });
         }
       });
     })
