a89312f987ab90b8c926db4796319af3553346c0
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2025-08-01 18:17:05 +0530

    Update shed_order_mao_2025.js

--- js/shed_order_mao_2025.js
+++ js/shed_order_mao_2025.js
@@ -2,12 +2,22 @@ window.shedOrdersByYear = window.shedOrdersByYear || {};
 window.shedOrdersByYear["2025"] = [
   {
     number: "01/2025",
-    topic: "Safety Guidelines for Monsoon",
+    topic: "Joint Procedure to keep LC No : 50 between LOL- AT closed after 19:00 to morning 07:00",
     file: "01_2025.pdf"
   },
   {
     number: "02/2025",
-    topic: "Loco Operations in Rain",
+    topic: "Correction Slip No : 02 to Non-monsoon Working Time Table No : 32 Part B 2024",
     file: "02_2025.pdf"
+  },
+  {
+    number: "03/2025",
+    topic: "Maintenance and Operation of NMG Coaches",
+    file: "03_2025.pdf"
+  },
+  {
+    number: "04/2025",
+    topic: "Action plan to avoid SPAD cases in Konkan Railway ",
+    file: "04_2025.pdf"
   }
 ];
