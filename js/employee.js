54191c159468b9a2ea4d078bf46049894b6632e6
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2026-01-08 10:17:31 +0530

    Delete js/employee.js

--- js/employee.js
+++
@@ -1,6 +0,0 @@
-const employees = [
-  { empNo: "4025", password: "pass4025", name: "Sajeesh P", designation: "LP" },
-  { empNo: "4026", password: "pass4026", name: "Shajithomas", designation: "LP" },
-  { empNo: "7500", password: "pass7500", name: "Suresha", designation: "ALP" },
-  // Add more employees as needed
-];
